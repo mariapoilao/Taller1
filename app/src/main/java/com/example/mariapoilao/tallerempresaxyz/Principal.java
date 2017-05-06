75efa7ed0ae13e322f2d2a9ac01d9e69bb09f342
Maria <mariapoilao@Marias-MacBook-Pro.local>
2017-05-06 13:53:40 -0500

    Primera fase

---
+++ app/src/main/java/com/example/mariapoilao/tallerempresaxyz/Principal.java
@@ -0,0 +1,13 @@
+package com.example.mariapoilao.tallerempresaxyz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
